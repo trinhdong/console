6d05875e77119d7ac66eeee72b60992ba21caa19
trinhdong <trinhvandong4479@gmail.com>
2019-05-29 15:39:51 +0700

    design database

---
+++ database/migrations/2019_05_29_053038_create_product_types_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('type_name');
+            $table->integer('category_id')->unsigned();
+            $table->index('category_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_types');
+    }
+}
--- database/migrations/2019_05_29_082632_create_admin_users_table.php
+++ database/migrations/2019_05_29_082632_create_admin_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateAdminUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,18 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admin_users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->date('birthday')->nullable();
-            $table->string('address')->nullable();
-            $table->integer('age')->nullable();
-            $table->boolean('sex')->nullable();
-            $table->integer('phone')->nullable();
-            $table->rememberToken();
+            $table->integer('is_admin')->default(0);
             $table->timestamps();
         });
     }
@@ -36,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admin_users');
     }
 }
