a183be956290592d7627ee5328625f7a4fb2459a
Hieu Nguyen <hieunld@imt-soft.com>
2019-08-03 22:42:07 +0700

    final

---
+++ resources/views/page/test.blade.php
@@ -0,0 +1,17 @@
+<div class="row">
+                    <div class="col-lg-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">With Multi iteam</div>
+                            <div class="panel-wrapper p-b-10 collapse in">
+                                <div id="owl-demo2" class="owl-carousel owl-theme">
+                                    <div class="item"><img src="../plugins/images/big/img1.jpg" alt="Owl Image"></div>
+                                    <div class="item"><img src="../plugins/images/big/img2.jpg" alt="Owl Image"></div>
+                                    <div class="item"><img src="../plugins/images/big/img3.jpg" alt="Owl Image"></div>
+                                    <div class="item"><img src="../plugins/images/big/img4.jpg" alt="Owl Image"></div>
+                                    <div class="item"><img src="../plugins/images/big/img5.jpg" alt="Owl Image"></div>
+                                    <div class="item"><img src="../plugins/images/big/img6.jpg" alt="Owl Image"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
